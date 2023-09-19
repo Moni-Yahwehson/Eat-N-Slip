8b9c8400c62402e4d66e238ef7f7881c6e0dcb26
Moni-Yahwehson <moniyahwehson@gmail.com>
2023-09-19 14:01:02 +0100

    Added More input field text

--- src/App.js
+++ src/App.js
@@ -89,6 +89,17 @@ function FormSplitBill() {
   return (
     <form className="form-split-bill">
       <h2>Split a bill with X </h2>
+
+      <label>Bill value</label>
+      <input type="text" />
+
+      <label>Your expense</label>
+      <input type="text" />
+
+      <label>X's expense</label>
+      <input type="text" disabled />
+
+      <Button>Split bill</Button>
     </form>
   );
 }
